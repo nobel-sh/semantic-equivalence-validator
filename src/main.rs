b73f30c45faeeaba5ea60816f9f0b0b39daa62a8
nobel <nobel2073@gmail.com>
2024-07-22 23:19:03 +0545

    feat: add support for cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+
+use crate::cli::{Cli, CliError, Mode};
+use clap::Parser;
+use std::{fs, process::ExitCode};
+
+fn main() -> ExitCode {
+    if let Err(e) = handle_cli() {
+        eprintln!("{}", e);
+        return ExitCode::FAILURE;
+    }
+    ExitCode::SUCCESS
 }
+
+pub fn handle_cli() -> Result<(), CliError> {
+    let args = Cli::parse();
+
+    match args.mode {
+        Mode::Test { filename } => {
+            if !filename.exists() {
+                return Err(CliError::InvalidPath(format!(
+                    "file '{}' does not exist",
+                    filename.display()
+                )));
+            }
+            let _ = fs::read_to_string(filename)?;
+        }
+    }
+    Ok(())
+}
+

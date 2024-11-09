3b93ce7df0108e63ea405798d18b0a8d9c450bfa
nobel <nobel2073@gmail.com>
2024-11-09 22:10:10 +0545

    test: case to verify size of structs

---
+++ tests/gccrs/struct2.rs
@@ -0,0 +1,133 @@
+#![feature(intrinsics)]
+#[lang = "sized"]
+pub trait Sized {}
+
+extern "C" {
+    fn printf(format: *const i8, ...);
+}
+
+extern "rust-intrinsic" {
+    fn size_of<T>() -> usize;
+}
+
+#[repr(C)]
+struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[repr(C)]
+struct StructC {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+#[repr(packed)]
+struct StructPacked {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+#[repr(align(16))]
+struct StructAligned {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+#[repr(transparent)]
+struct StructTransparent(StructC);
+
+struct StructDefault {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+fn verify_sizeof() {
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    let matrix = [[1.0; 4]; 4];
+    let data = [0u8; 16];
+
+    let struct_c = StructC {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    let struct_packed = StructPacked {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    let struct_aligned = StructAligned {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+
+    unsafe {
+        printf("=== Sizes ===\n\0" as *const str as *const i8);
+        printf(
+            "Vector3: %zu\n\0" as *const str as *const i8,
+            size_of::<Vector3>(),
+        );
+        printf(
+            "StructC: %zu\n\0" as *const str as *const i8,
+            size_of::<StructC>(),
+        );
+        printf(
+            "StructPacked: %zu\n\0" as *const str as *const i8,
+            size_of::<StructPacked>(),
+        );
+        printf(
+            "StructAligned: %zu\n\0" as *const str as *const i8,
+            size_of::<StructAligned>(),
+        );
+        printf(
+            "StructTransparent: %zu\n\0" as *const str as *const i8,
+            size_of::<StructTransparent>(),
+        );
+        printf(
+            "StructDefault: %zu\n\0" as *const str as *const i8,
+            size_of::<StructDefault>(),
+        );
+    }
+}
+
+fn main() -> i32 {
+    verify_sizeof();
+    0
+}
---
+++ tests/rustc/struct2.rs
@@ -0,0 +1,117 @@
+use std::mem::size_of;
+
+#[repr(C)]
+struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[repr(C)]
+struct StructC {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+#[repr(packed)]
+struct StructPacked {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+#[repr(align(16))]
+struct StructAligned {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+#[repr(transparent)]
+struct StructTransparent(StructC);
+
+struct StructDefault {
+    flag: bool,
+    number: i32,
+    vector: Vector3,
+    matrix: [[f32; 4]; 4],
+    data: [u8; 16],
+}
+
+fn verify_sizeof() {
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    let matrix = [[1.0; 4]; 4];
+    let data = [0u8; 16];
+
+    let struct_c = StructC {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    let struct_packed = StructPacked {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+    let struct_aligned = StructAligned {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+
+    let vector = Vector3 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+    };
+
+    let struct_c = StructC {
+        flag: true,
+        number: 42,
+        vector,
+        matrix,
+        data,
+    };
+    let struct_transparent = StructTransparent(struct_c);
+
+    println!("=== Sizes ===");
+    println!("Vector3: {}", size_of::<Vector3>());
+    println!("StructC: {}", size_of::<StructC>());
+    println!("StructPacked: {}", size_of::<StructPacked>());
+    println!("StructAligned: {}", size_of::<StructAligned>());
+    println!("StructTransparent: {}", size_of::<StructTransparent>());
+    println!("StructDefault: {}", size_of::<StructDefault>());
+}
+
+fn main() {
+    verify_sizeof();
+}
